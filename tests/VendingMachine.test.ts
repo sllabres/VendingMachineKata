c7fb5df41866b5ed8cb42f5eb3ffdafa79307925
sllabres <sllabres@hotmail.com>
2020-01-17 11:35:45 +0000

    move setup outside test

--- tests/VendingMachine.test.ts
+++ tests/VendingMachine.test.ts
@@ -1,11 +1,16 @@
 import { expect } from "chai";
 
-describe('no coin inserted', function () {
-  it('displays "INSERT COIN" message', function () {
-    var displayFake = new DisplayFake();
-    var vendingMachine = new VendingMachine(displayFake);    
+describe('given no coin inserted', function () {
+  it('displays "INSERT COIN" message', function () {    
     vendingMachine.vend();
     expect(displayFake.CurrentMessage).equals(Message.NoCoin);
+  });
+});
+
+describe('given nickel inserted', function () {
+  it('displays "$0.05" message', function () {
+    vendingMachine.insertCoin();
+    expect(displayFake.CurrentMessage).equals("$0.05");
   });
 });
 
@@ -13,24 +18,31 @@ enum Message {
   NoCoin = "INSERT COIN"
 }
 
-class VendingMachine{
-  display: IDisplay;  
-  constructor(display: IDisplay) {    
+class VendingMachine {
+  display: IDisplay;
+  constructor(display: IDisplay) {
     this.display = display;
   }
 
   public vend(): void {
     this.display.update(Message.NoCoin);
   }
+
+  public insertCoin(): void {
+    this.display.update("$0.05");
+  }
 }
 
 interface IDisplay {
-  update(message: string) : void;
+  update(message: string): void;
 }
 
 class DisplayFake {
   public CurrentMessage: string = "";
-  public update(message: string) : void {
+  public update(message: string): void {
     this.CurrentMessage = message;
   }
 }
+
+var displayFake = new DisplayFake();
+var vendingMachine = new VendingMachine(displayFake);
