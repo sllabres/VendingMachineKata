9a058db5a0a5749fdf266b7d4b68dbc10c6d91ad
sllabres <sllabres@hotmail.com>
2020-01-17 11:25:46 +0000

    no coin inserted test

--- tests/VendingMachine.test.ts
+++ tests/VendingMachine.test.ts
@@ -1,7 +1,32 @@
 import { expect } from "chai";
 
-describe('passing setup test', function () {
-  it('true is true', function () {
-    expect(true).equal(true);
+describe('no coin inserted', function () {
+  it('displays "INSERT COIN" message', function () {
+    var displayFake = new DisplayFake();
+    var vendingMachine = new VendingMachine(displayFake);    
+    vendingMachine.vend();
+    expect(displayFake.CurrentMessage).equals("INSERT COIN");
   });
 });
+
+class VendingMachine{
+  display: IDisplay;  
+  constructor(display: IDisplay) {    
+    this.display = display;
+  }
+
+  public vend(): void {
+    this.display.update("INSERT COIN");
+  }
+}
+
+interface IDisplay {
+  update(message: string) : void;
+}
+
+class DisplayFake {
+  public CurrentMessage: string = "";
+  public update(message: string) : void {
+    this.CurrentMessage = message;
+  }
+}
