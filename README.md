5606454183a65c58aa7897a11f940b6acc1152ad
sllabres <sllabres@hotmail.com>
2020-01-17 11:20:32 +0000

    first commit - setup

---
+++ README.md
@@ -0,0 +1,2 @@
+# Vending Machine Kata
+Command - NPM RUN TEST
---
+++ tests/VendingMachine.test.ts
@@ -0,0 +1,7 @@
+import { expect } from "chai";
+
+describe('passing setup test', function () {
+  it('true is true', function () {
+    expect(true).equal(true);
+  });
+});
